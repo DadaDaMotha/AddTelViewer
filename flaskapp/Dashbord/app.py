2a8d347218f7d2cc67a3cdc20ade4d8036c975ed
Lukas Burkhard <dadadamotha@gmail.com>
2019-06-01 22:37:56 +0200

    making things simpler
    - layout is dashboard specific
    - customizing Dash with one function is simple

--- flaskapp/Dashbord/app.py
+++ flaskapp/Dashbord/app.py
@@ -1,32 +1,89 @@
-from DashCustom.custom_class import DashResponsive
 from server import application
+import dash
+class DashResponsive(dash.Dash):
+    def interpolate_index(self, **kwargs):
+        return '''
+        <!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <title>AddTelViewer</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link href="https://getbootstrap.com/docs/4.1/examples/dashboard/dashboard.css" rel="stylesheet">
+    <link href="/static/custom_css.css" rel="stylesheet">
+    <link href="/static/load_ctrl_clr.css" rel="stylesheet">
+</head>
+
+<body>
+    <nav class="navbar navbar-light fixed-top bg-light flex-md-nowrap p-0 shadow">
+        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="/">PLZ Finder</a>
+        <ul class="navbar-nav px-3">
+            <li class="nav-item text-nowrap">
+                <a class="nav-link" href="/login">Einloggen</a>
+            </li>
+        </ul>
+
+    </nav>
+
+    <div class="container-fluid">
+        <div class="row">
+            <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+                <div class="sidebar-sticky">
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link icyLink" href="/">
+                                <span data-feather="map-pin"></span>
+                                GeoTools<span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link icyLink" href="/telscraper/">
+                                <span data-feather="book-open"></span>
+                                TelScraper<span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link icyLink" href="/info/">
+                                <span data-feather="info"></span>
+                                Info
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+            <main role="main" class="col-md-9 ml-sm-auto col-lg-10 ">
+                {app_entry}
+            </main>
+        </div>
+    </div>
+    {config}
+    {scripts}
+    {renderer}
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
+    <script>
+      feather.replace()
+    </script>    
+</body>
+</html>
+        '''.format(
+            app_entry=kwargs.get('app_entry'),
+            config=kwargs.get('config'),
+            scripts=kwargs.get('scripts'),
+            renderer=kwargs.get('renderer'))
+
 
 app = DashResponsive(name="dashboard", sharing=True, server=application, url_base_pathname='/')
 app.config.supress_callback_exceptions = True
 
-# external_css = ['/static/custom_css.css',
-#                 '/static/load_ctrl_clr.css'
-# ]
-# for css in external_css:
-#     app.css.append_css({"external_url": css})
-#
-# external_scripts = ['https://code.jquery.com/jquery-3.3.1.slim.min.js',
-#                     'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js',
-#                     'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js'
-# ]
-#
-
-# for script in external_scripts:
-#     app.scripts.append_script({"external_url": script})
-
-# Use this if there is no internet connection
-
-# app.css.config.serve_locally = True
-# app.scripts.config.serve_locally = True
-
-# @app.server.route('/static/<path:path>')
-# def static_file(path):
-#     static_folder = os.path.join(basedir, 'static')
-#     return send_from_directory(static_folder, path)
 
 

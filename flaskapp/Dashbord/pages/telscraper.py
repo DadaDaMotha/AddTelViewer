ad32794aa58f27b644efc09e612e210fc4871d3c
Lukas Burkhard <dadadamotha@gmail.com>
2019-06-01 20:40:14 +0200

    add option to output for geocoding

--- flaskapp/Dashbord/pages/telscraper.py
+++ flaskapp/Dashbord/pages/telscraper.py
@@ -8,66 +8,92 @@ from Dashbord.app import app
 from six.moves.urllib.parse import quote
 import datetime as dt
 
-
 from packages.tel_scraper import LocalCH, TelSearch
+from packages.tel_scraper.LocalCH.model import db_cols as lch_cols
+from packages.tel_scraper.TelSearch.model import db_cols as tls_cols
+from packages.utils import add_mmqgis_fields
+
 
 layout = html.Div(className='container mt-4', children=[
 
-            html.H2('Search Local CH and TelSearch'),
+    html.H2('Search Local CH and TelSearch'),
 
+    html.Div(className='row my-4', children=[
+        html.Div(className='col-sm-2', children=[
+            dcc.Dropdown(id='source-dropdown', className='mt',
+                         options=[{'label': x, 'value': x} for x in ['TelSearch', 'LocalCH']], value='LocalCH',
+                         clearable=False)
+        ]),
 
-            html.Div(className='row my-4', children=[
-                html.Div(className='col-sm-2', children=[
-                    dcc.Dropdown(id='source-dropdown', className='mt',
-                             options=[{'label': x, 'value': x} for x in ['TelSearch', 'LocalCH']], value='LocalCH',
-                 clearable=False)
-                ]),
+        html.Div(className='col-sm-3', children=[
+            dcc.Input(id="was-query", className='form-control form-control mt', type="text", size=20,
+                      placeholder="Was...")
+        ]),
+        html.Div(className='col-sm-3', children=[
+            dcc.Input(id="wo-query", className='form-control form-control mt', type="text", size=20,
+                      placeholder="Wo...")
+        ]),
+        html.Div(className='col-sm-2', children=[
+            dcc.Dropdown(id='cat-dropdown', className='mt',
+                         options=[{'label': x, 'value': x} for x in ['Private', 'Firmen']], value='',
+                         clearable=True, placeholder='Kategorie: Beide')
+        ]),
+    ]),
+    html.Div(className='row my-4', children=[
+        html.Div(className='col-sm-3', children=[
+            html.Button(id="button-query", className='btn btn-outline-info btn-primary mt', children="SUCHEN",
+                        n_clicks=0),
+        ]),
+    ]),
+    html.Div(className='row my-4', children=[
+        html.Div(id='table-div', className='col-sm-12', children=[
 
-                html.Div(className='col-sm-3', children=[
-                    dcc.Input(id="was-query", className='form-control form-control mt', type="text", size=20,
-                              placeholder="Was...")
-                ]),
-                html.Div(className='col-sm-3', children=[
-                    dcc.Input(id="wo-query", className='form-control form-control mt', type="text", size=20,
-                              placeholder="Wo...")
-                ]),
-                html.Div(className='col-sm-2', children=[
-                dcc.Dropdown(id='cat-dropdown', className='mt',
-                             options=[{'label': x, 'value': x} for x in ['Private', 'Firmen']], value='',
-                 clearable=True, placeholder='Kategorie: Beide')
-                ]),
+            html.Div(className='mt', children=[
+                dte.DataTable(
+                    id='datatable-query',
+                    columns=[{}],
+                    data=[{}],
+                    style_table={'width': '100%'},
+                    content_style='grow',
+                    style_cell={'text-align': 'left', 'padding-left': '1em'},
+                    style_as_list_view=True
+
+                )
             ]),
-                html.Div(className='row my-4', children=[
-                html.Div(className='col-sm-3', children=[
-                    html.Button(id="button-query", className='btn btn-outline-info btn-primary mt', children="SUCHEN",
-                                n_clicks=0),
-                ]),
-            ]),
-            html.Div(className='row my-4', children=[
-                html.Div(id='table-div', className='col-sm-12', children=[
-
-                    html.Div(className='mt', children=[
-                        dte.DataTable(
-                            id='datatable-query',
-                            columns=[{}],
-                            data=[{}],
-                            style_table={'width': '100%'},
-                            content_style='grow',
-                            style_cell={'text-align':'left', 'padding-left': '1em'},
-                            style_as_list_view=True
-
-                        )
-                    ]),
-                    html.A('DOWNLOAD',
-                            id='download-link',
-                            download="localch_{}.csv".format(dt.datetime.now().strftime('%Y_%m_%d_%H%M')),
-                            href="",
-                            target="_blank",
-                            className='btn btn-info btn-primary mt-4'
-                        ),
-                ])
-            ])
         ])
+    ]),
+    html.Div(className='row my-4', children=[
+        html.Div(className='col-sm-12', children=[
+            html.Div(
+                style={'alignItems': 'center'},
+                className='d-flex flex-row',
+                children=[
+                    html.A('DOWNLOAD',
+                           id='download-link',
+                           download="localch_{}.csv".format(dt.datetime.now().strftime('%Y_%m_%d_%H%M')),
+                           href="",
+                           target="_blank",
+                           className='btn btn-info btn-primary'
+                           ),
+                    html.Span('OutputFormat', className='ml-4'),
+                    html.Div(style={'width': '100px'}, children=[
+                        dcc.Dropdown(
+                            id="output-fmt",
+                            options=[
+                                {'label': 'csv', 'value': 'csv'},
+                                {'label': 'csv, mmqgis fields', 'value': 'mmqgis'},
+                            ],
+                            value='csv',
+                            clearable=False,
+                            className='ml-2',
+                            style={'minWidth': '12em'}
+                        ),
+                    ]),
+                ]),
+        ]),
+    ])
+])
+
 
 @app.callback(
     [Output('datatable-query', 'columns'), Output('datatable-query', 'data')],
@@ -75,7 +101,6 @@ layout = html.Div(className='container mt-4', children=[
     state=[State('was-query', 'value'), State('wo-query', 'value'),
            State('cat-dropdown', 'value'), State('source-dropdown', 'value')])
 def get_data(n_clicks, was, wo, cat, source):
-
     if (n_clicks > 0) and (was or wo):
 
         print('type wo', type(wo))
@@ -91,38 +116,34 @@ def get_data(n_clicks, was, wo, cat, source):
 
         if source == 'LocalCH':
             df_query = LocalCH.page_aggregator(query_dict, max_pages=5)
-            df_query = df_query[df_query.columns.difference(['Betriebsart'])]
+            cols = lch_cols
         elif source == 'TelSearch':
             df_query = TelSearch.page_aggregator(query_dict, max_pages=5)
+            cols = tls_cols
         else:
             return [{}]
 
-        df = df_query[df_query.columns.difference(['URL', 'query'])]
-        columns = [{'name': i, 'id': i} for i in df.columns]
+        # cols = df_query.columns.difference(['URL', 'query'])
+        df = df_query[cols]
+        columns = [{'name': i, 'id': i} for i in cols]
 
         return columns, df.to_dict('records')
     else:
-        # return [{'id':'column-1', 'name':'number'}, {'id':'column-2', 'name':'city'},
-        #                              {'id':'column-3', 'name':'country'}],\
-        #        [{'column-1': 4.5, 'column-2': 'montreal', 'column-3': 'canada'},
-        #                           {'column-1': 8, 'column-2': 'boston', 'column-3': 'america'}]
         return [{}], [{}]
+
 
 @app.callback(
     Output('download-link', 'href'),
-    [Input('datatable-query', 'derived_viewport_data')])
-def update_download_link(rows):
-    if not rows == [{}]:
-        dff = pd.DataFrame.from_dict(rows, 'columns')
-        csv_string = dff.to_csv(index=False, encoding='utf-8', sep=';')
-        csv_string = "data:text/csv;charset=utf-8,%EF%BB%BF" + quote(csv_string)
-        return csv_string
-    return ""
+    [Input('datatable-query', 'derived_viewport_data')],
+    state=[State('output-fmt', 'value')]
+)
+def update_download_link(rows, output_fmt):
+    if rows == [{}] or rows is None:
+        return ""
+    dff = pd.DataFrame.from_dict(rows, 'columns')
+    if output_fmt == 'mmqgis':
+        dff = add_mmqgis_fields(dff)
+    csv_string = dff.to_csv(index=False, encoding='utf-8', sep=';')
+    csv_string = "data:text/csv;charset=utf-8,%EF%BB%BF" + quote(csv_string)
+    return csv_string
 
-#
-# @app.callback(
-#     Output('geopy-search', 'value'),
-#     [Input('choose-plz', 'value')])
-#
-# def clear_search_field(plz):
-#     return []
--- flaskapp/packages/utils.py
+++ flaskapp/packages/utils.py
@@ -1,14 +1,17 @@
 import json
 
+
 def JSON_write(config, to_file):
 
     with open(to_file, 'w') as f:
         json.dump(config, f)
 
+
 def JSON_update(config, to_file):
 
     with open(to_file, 'a') as f:
         json.dump(config, f)
+
 
 def JSON_read(from_file):
 
@@ -17,11 +20,34 @@ def JSON_read(from_file):
         f.close()
         return config
 
+
 def func_name():
     import traceback
     return traceback.extract_stack(None, 2)[0][2]
+
 
 def read_file_as_str(filepath):
     with open(filepath) as f:
         res = f.read()
     return res
+
+
+def add_mmqgis_fields(df):
+    '''
+
+    :param df: take in a dataframe from page_aggregator function of LocalCH or TelSearch
+    :return: re-arranged dataframe with different columns headers and collapsed address
+
+    Specs: needs address, city, state, country
+    mmqgis working examples:
+
+        Rautistrasse 33 | Zuerich | Zuerich |	Schweiz
+        Rautistrasse 33 | 8047 Zuerich | Zuerich |	Schweiz
+
+    '''
+
+    dff = df.copy(deep=True)
+    dff['address'] = df[['Strasse', 'Hausnummer']].apply(lambda x: ' '.join(x), axis=1)
+    dff['city'] = df[['PLZ', 'Ort']].apply(lambda x: ' '.join(x), axis=1)
+    dff['country'] = 'Schweiz'
+    return dff
